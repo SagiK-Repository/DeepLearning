8258b4957c859264692148fd8a5f8d29f19ce962
juhyung1021 <juhyung1021@gamil.com>
2024-01-20 20:46:52 +0900

    feat:class로 전환

--- 인공뉴런/Perceptron/CPP/getData.h
+++ 인공뉴런/Perceptron/CPP/getData.h
@@ -6,112 +6,6 @@
 #include <algorithm>
 #include <random>
 
-// Seed First
-std::vector<float> Normal_Distribution_Vector(int size, int seed);
-
-template <int N>
-std::array<float, N> Normal_Distribution_Array(int seed);
-
-std::vector<float> Uniform_Distribution_Vector(int size, int seed);
-
-template <int N>
-std::array<float, N> Uniform_Distribution_Array(int seed);
-
-// 표준편차 데이터 획득
-std::vector<float> Normal_Distribution_Vector(int size, float loc = 0.0, float scale = 0.01, int seed = 1);
-
-template <int N>
-std::array<float, N> Normal_Distribution_Array(float loc = 0.0, float scale = 0.01, int seed = 1);
-
-// 균일분포 데이터 획득
-std::vector<float> Uniform_Distribution_Vector(int size, float min = 0.0, float max = 1.0, int seed = 1);
-
-template <int N>
-std::array<float, N> Uniform_Distribution_Array(float min = 0.0, float max = 1.0, int seed = 1);
-
-
-// 표준편차 데이터 획득
-std::vector<float> Normal_Distribution_Vector(int size, float loc, float scale, int seed) {
-    std::vector<float> result;
-    std::random_device rd;
-    std::mt19937 gen(seed);
-    std::normal_distribution<double> dist(loc, scale);
-    double randomNumber;
-
-    for(int repeat_num = 0; repeat_num < size; repeat_num++){
-        randomNumber = dist(gen);
-        result.push_back(randomNumber);
-    }
-
-    return result;
-}
-
-template <int N>
-std::array<float, N> Normal_Distribution_Array(float loc, float scale, int seed) {
-    std::array<float, N> result;
-    std::random_device rd;
-    std::mt19937 gen(seed);
-    std::normal_distribution<double> dist(loc, scale);
-    double randomNumber;
-
-    for(int repeat_num = 0; repeat_num < N; repeat_num++){
-        randomNumber = dist(gen);
-        result[repeat_num] = randomNumber;
-    }
-
-    return result;
-}
-
-// 균일분포(uniform distribution) 데이터 획득
-std::vector<float> Uniform_Distribution_Vector(int size, float min, float max, int seed) {
-    std::vector<float> result;
-    std::default_random_engine engine(seed);
-    std::uniform_real_distribution<double> uniformDist(min, max);
-    double randomNumber;
-
-    for(int repeat_num = 0; repeat_num < size; repeat_num++){
-        randomNumber = uniformDist(engine);
-        result.push_back(randomNumber);
-    }
-
-    return result;
-}
-
-template <int N>
-std::array<float, N> Uniform_Distribution_Array(float min, float max, int seed) {
-    std::array<float, N> result;
-    std::default_random_engine engine(seed);
-    std::uniform_real_distribution<double> uniformDist(min, max);
-    double randomNumber;
-
-    for(int repeat_num = 0; repeat_num < N; repeat_num++){
-        randomNumber = uniformDist(engine);
-        result[repeat_num] = randomNumber;
-    }
-
-    return result;
-}
-
-// Seed First
-std::vector<float> Normal_Distribution_Vector(int size, int seed) {
-    return Normal_Distribution_Vector(size, 0.0f, 0.01f, seed);
-}
-template <int N>
-std::array<float, N> Normal_Distribution_Array(int seed) {
-    return Normal_Distribution_Array<N>(0.0f, 0.01f, seed);
-}
-std::vector<float> Uniform_Distribution_Vector(int size, int seed) {
-    return Uniform_Distribution_Vector(size, 0.0f, 1.0f, seed);
-}
-
-template <int N>
-std::array<float, N> Uniform_Distribution_Array(int seed) {
-    return Uniform_Distribution_Array<N>(0.0f, 1.0f, seed);
-}
-
-
-// Class --------------------------------------------------------------------------------
-
 // Interface
 class IRandomeVector {
 public:
@@ -137,7 +31,18 @@ public:
     Normal_Distribution_Vector_Class(int size = 100, float loc = 0.0, float scale = 0.01, int seed = 1) : seed(seed), size(size), loc(loc), scale(scale) { }
 
     std::vector<float> getData() override {
-        return Normal_Distribution_Vector(size, loc, scale, seed);
+        std::vector<float> result;
+        std::random_device rd;
+        std::mt19937 gen(seed);
+        std::normal_distribution<double> dist(loc, scale);
+        double randomNumber;
+
+        for(int repeat_num = 0; repeat_num < size; repeat_num++){
+            randomNumber = dist(gen);
+            result.push_back(randomNumber);
+        }
+
+        return result;
     }
 };
 
@@ -153,7 +58,17 @@ public:
     Uniform_Distribution_Vector_Class(int size = 100, int seed = 1, float min = 0.0, float max = 1.0) : seed(seed), size(size), min(min), max(max) { }
 
     std::vector<float> getData() override {
-        return Uniform_Distribution_Vector(size, min, max, seed);
+        std::vector<float> result;
+        std::default_random_engine engine(seed);
+        std::uniform_real_distribution<double> uniformDist(min, max);
+        double randomNumber;
+
+        for(int repeat_num = 0; repeat_num < size; repeat_num++){
+            randomNumber = uniformDist(engine);
+            result.push_back(randomNumber);
+        }
+
+        return result;
     }
 };
 
@@ -169,7 +84,18 @@ public:
     Normal_Distribution_Array_Class(float loc = 0.0, float scale = 0.01, int seed = 1) : seed(seed), loc(loc), scale(scale) { }
 
     std::array<float, N> getData() override {
-        return Normal_Distribution_Array<N>(loc, scale, seed);
+        std::array<float, N> result;
+        std::random_device rd;
+        std::mt19937 gen(seed);
+        std::normal_distribution<double> dist(loc, scale);
+        double randomNumber;
+
+        for(int repeat_num = 0; repeat_num < N; repeat_num++){
+            randomNumber = dist(gen);
+            result[repeat_num] = randomNumber;
+        }
+
+        return result;
     }
 };
 
@@ -185,6 +111,16 @@ public:
     Uniform_Distribution_Array_Class(int seed = 1, float min = 0.0, float max = 1.0) : seed(seed), min(min), max(max) { }
 
     std::array<float, N> getData() override {
-        return Uniform_Distribution_Array<N>(min, max, seed);
+        std::array<float, N> result;
+        std::default_random_engine engine(seed);
+        std::uniform_real_distribution<double> uniformDist(min, max);
+        double randomNumber;
+
+        for(int repeat_num = 0; repeat_num < N; repeat_num++){
+            randomNumber = uniformDist(engine);
+            result[repeat_num] = randomNumber;
+        }
+
+        return result;
     }
 };
