060a3343cde0a23e191ee24db87d21d480de8cec
juhyung1021 <juhyung1021@gamil.com>
2024-01-27 07:42:09 +0900

    feat ; perceptron cpp

---
+++ 인공뉴런/Perceptron/CPP/Perceptron.cpp
@@ -0,0 +1,43 @@
+//     set     :      cd '/workspace/인공뉴런/Perceptron/CPP'
+//   build     :      g++ ./Perceptron.cpp -o Perceptron_cpp
+//     run     :      ./Perceptron_cpp
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <random>
+#include "csvReader.h"
+#include "getData.h"
+
+// Main
+int main() {
+    // Data Loading
+    std::string filename = "data.csv";
+    int seed = 1;
+
+    // =========== Vector =================
+    std::cout << "= Vector =" << std::endl;
+    std::vector<std::vector<float>> matrix = processCSV_vector(filename);
+    matrix.erase(matrix.begin()); // 첫 번 데이터는 x
+
+
+    // =========== Array ==================
+    std::cout << "= Array =" << std::endl;
+    std::array<std::array<float, 2>, 101> result = processCSV_array<101>(filename);
+
+    std::array<std::array<float, 2>, 100> newResult;
+    std::copy(result.begin() + 1, result.end(), newResult.begin()); // 첫 번 데이터는 x
+
+    std::array<float, 100> normalArray = Normal_Distribution_Array<100>(seed);
+
+
+    // Data Loading Test
+    // std::cout << "10th value: " << matrix[99][0] << ", " << matrix[99][1] << std::endl;
+    // std::cout << "10th value: " << result[100][0] << ", " << result[100][1] << std::endl;
+
+    return 0;
+}
--- 인공뉴런/Perceptron/CPP/Perceptron_Test.cpp
+++ 인공뉴런/Perceptron/CPP/Perceptron_Test.cpp
@@ -1,20 +1,28 @@
 // cd '/workspace/인공뉴런/Perceptron/CPP'
-// g++ -std=c++11 -o getData_Test getData_Test.cpp
-// ./getData_Test
+// g++ -std=c++11 -o Perceptron_Test Perceptron_Test.cpp
+// ./Perceptron_Test
 
 #define CATCH_CONFIG_MAIN
 #include "/workspace/CPP Unit Test/catch.hpp"
 
 #include "Perceptron.h"
+#include "csvReader.h"
+#include "getData.h"
 
-TEST_CASE("Perceptron", "[Perceptron]") {
+TEST_CASE("Perceptron_Vector", "[Perceptron_Vector]") {
     // Arrange
-    Normal_Distribution getData;
-    Perceptron(0.1, 10, &getData);
+    std::string filename = "data.csv";
+    std::vector<std::vector<float>> input_data = processCSV_vector(filename);
+    IRandomeVector* randomVectorPtr = new Normal_Distribution_Vector_Class(2, 1);
+    IPerceptron_Vector* perceptronvector = new Perceptron_Vector(randomVectorPtr);
+    std::vector<float> result_data;
+    for(int i = 0; i < input_data.size(); i++)
+        result_data.push_back((i >= 50)?1:0);
+    std::vector<float> myVector = {1, 2};
 
     // Act
-    Perceptron.fit(X, y);
+    perceptronvector.fit(input_data, result_data);
 
     // Assert
-    // REQUIRE(Rounding(normalArray[0],4) == -0.0055f);
+    REQUIRE(perceptronvector.Predict(myVector) == 1);
 }
