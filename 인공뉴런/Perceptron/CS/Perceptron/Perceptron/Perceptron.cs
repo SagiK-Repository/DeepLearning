2c878461caef0b0eae1b03262b162d87610c3b23
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-01-27 22:15:28 +0900

    style : percepton cs code edit

--- 인공뉴런/Perceptron/CS/Perceptron/Perceptron/Perceptron.cs
+++ 인공뉴런/Perceptron/CS/Perceptron/Perceptron/Perceptron.cs
@@ -6,7 +6,6 @@ public interface IPerception
     public float LearningRate { get; }
     public int Epoch { get; }
     public int Random_state { get; }
-
 }
 
 public class Perceptron : IPerception
@@ -22,9 +21,9 @@ public class Perceptron : IPerception
     {
         Errors = new List<int>();
         Weight = new List<float>();
-        this.LearningRate = learningRate;
-        this.Epoch = epoch;
-        this.Random_state = random_state;
+        LearningRate = learningRate;
+        Epoch = epoch;
+        Random_state = random_state;
     }
 
     public void Initailize(List<List<float>> input_data)
@@ -39,19 +38,26 @@ public class Perceptron : IPerception
         for (int e = 0; e < Epoch; e++)
         {
             int error = 0;
-            var dataAndResult = input_data.Zip(result_data, (data, result) => (data, result));
-            foreach (var (data, result) in dataAndResult)
-            {
-                float delta = LearningRate * (result - Predict(data));
-                for(int i = 0; i < data.Count(); i++)
-                    Weight[i] += data[i] * delta;
-                Bias = delta;
-                error += delta != 0.0f ? 1 : 0;
-            }
+            error = Training(input_data, result_data, error);
             Errors.Add(error);
-            Console.WriteLine($"-----epoch ({e+1}/{Epoch})-----error:{error}");
+            Console.WriteLine($"-----epoch ({e + 1}/{Epoch})-----error:{error}");
         }
     }
+
+    private int Training(List<List<float>> input_data, List<float> result_data, int error)
+    {
+        var dataAndResult = input_data.Zip(result_data, (data, result) => (data, result));
+        foreach (var (data, result) in dataAndResult)
+        {
+            float delta = LearningRate * (result - Predict(data));
+            Weight = Weight.Select((w, i) => w + data[i] * delta).ToList();
+            Bias = delta;
+            error += delta != 0.0f ? 1 : 0;
+        }
+
+        return error;
+    }
+
     public int Predict(List<float> input_data) => Caculate_Perceptron(input_data) >= 0.0f ? 1 : 0;
     public float Caculate_Perceptron(List<float> input_data) => Multiplex(input_data) + Bias;
     public float Multiplex(List<float> input_data) => input_data.Zip(Weight, (data, weight) => data * weight).Sum();
