d54d2ef267803e3f5308dfd99aebfebfbe1eedbd
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-01-27 15:47:43 +0900

    feat : add csv reader

---
+++ 인공뉴런/Perceptron/CS/Perceptron/Perceptron/CSVReader.cs
@@ -0,0 +1,20 @@
+﻿namespace CSVReader;
+
+public static class CSVReader
+{
+    public static string[][] Load(string filePath)
+    {
+        List<string[]> dataArray = new List<string[]>();
+
+        using (StreamReader sr = new StreamReader(filePath))
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] data = line.Split(',');
+
+                dataArray.Add(data);
+            };
+
+        return dataArray.ToArray();
+    }
+}
