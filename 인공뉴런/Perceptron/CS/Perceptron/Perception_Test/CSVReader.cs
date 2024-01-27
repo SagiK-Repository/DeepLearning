9da04162992cdb03d7a4ae7c0ec94eca6380998a
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-01-27 15:49:00 +0900

    test : add csv reader test

---
+++ 인공뉴런/Perceptron/CS/Perceptron/Perception_Test/CSVReader.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace Perception_Test;
+
+public class CSVReader_Test
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class TestCSVFile : BeforeAfterTestAttribute
+    {
+        private string FilePath = "test.csv";
+
+        public override void Before(MethodInfo methodUnderTest)
+        {
+            string[] csvData = new string[]
+            {
+            "John,Doe,john.doe@example.com",
+            "Jane,Smith,jane.smith@example.com",
+            };
+
+            File.WriteAllLines(FilePath, csvData);
+        }
+
+        public override void After(MethodInfo methodUnderTest)
+        {
+            File.Delete(FilePath);
+        }
+    }
+
+    [Fact]
+    [TestCSVFile]
+    public void CreateTestFile_Should_Exists()
+    {
+        // Assert
+        File.Exists("test.csv").Should().BeTrue();
+    }
+
+    [Fact]
+    [TestCSVFile]
+    public void Load_Should_Return_Correct_CSV_Data()
+    {
+        // Act
+        string[][] result = CSVReader.CSVReader.Load("test.csv");
+
+        // Assert
+        result[0][0].Should().Be("John");
+        result[1][1].Should().Be("Smith");
+        result[0][2].Should().Be("john.doe@example.com");
+    }
+}
